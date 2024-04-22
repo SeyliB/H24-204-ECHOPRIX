d008126822b62de7caa52b3ac39015e5b4488624
SeyliB <ilbelhaddad@outlook.com>
2024-04-21 23:03:43 -0400

    Transition a EJS

--- app.js
+++ app.js
@@ -4,27 +4,21 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT;
+const path = require('path');
 
-app.get('/accueil', (req, res) => {
-    // Define the data to be passed to the HTML file
-    const data = {
-      email: 'contact@example.com'
-    };
-  
-    // Define the path to the HTML file
-    const filePath = path.join(__dirname, 'public/html', 'accueil.html');
-  
-    // Read the HTML file and replace placeholders with data
-    res.sendFile(filePath, { 
-      // Specify the root object to replace placeholders
-      root: '.', 
-      // Set custom headers to specify content type
-      headers: {
-        'Content-Type': 'text/html'
-      }
-    });
-  });
 
+// Set 'public' directory as the static directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Images')));
+
+// Set 'views' directory for EJS templates
+app.set('views', path.join(__dirname, 'public', 'views'));
+app.set('view engine', 'ejs');
+        
+
+app.get('/', (req, res) => {
+    res.render('accueil');
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
