ae81572b8488d60e5e80b1d0debb139cabfe889d
SeyliB <ilbelhaddad@outlook.com>
2024-05-23 21:05:33 -0400

    vercel

--- start.bat
+++ start.bat
@@ -1,5 +1,22 @@
 @echo off
 
-pip install -r requirements.txt
+rem Check if the virtual environment directory exists
+if not exist ".venv" (
+    echo Virtual environment not found. Creating a new one...
+    python -m venv .venv
+)
 
-echo AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+rem Activate the virtual environment
+echo Activating virtual environment...
+call .\.venv\Scripts\activate
+
+rem Install dependencies from requirements.txt
+if exist "requirements.txt" (
+    echo Installing dependencies...
+    pip install -r requirements.txt
+) else (
+    echo requirements.txt not found.
+)
+
+rem Indicate that the process is complete
+echo Installation and setup complete.
